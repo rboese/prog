58d475e7f7082a31fa72b7a0a3a5505c53be9f23
rico.boese <rico.boese@valtech.de>
2017-02-01 14:35:53 +0100

    solution exercise 7

--- collections/src/main/java/collections/SortMe.java
+++ collections/src/main/java/collections/SortMe.java
@@ -1,11 +1,20 @@
 package collections;
 
+import java.util.Comparator;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+class StringBufferComperator implements Comparator<StringBuffer> {
+
+    @Override
+    public int compare(StringBuffer o1, StringBuffer o2) {
+        return o1.toString().compareTo(o2.toString());
+    }
+}
+
 public class SortMe {
     public static void main(String args[]) {
-        SortedSet<StringBuffer> s = new TreeSet<StringBuffer>();
+        SortedSet<StringBuffer> s = new TreeSet<>(new StringBufferComperator());
         s.add(new StringBuffer("Red"));
         s.add(new StringBuffer("White"));
         s.add(new StringBuffer("Blue"));
