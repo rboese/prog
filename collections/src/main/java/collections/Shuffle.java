ab1d107477e70c2a6919eaa959ccaede754ffa57
rico.boese <rico.boese@valtech.de>
2017-02-01 11:04:15 +0100

    solution exercise 2

--- collections/src/main/java/collections/Shuffle.java
+++ collections/src/main/java/collections/Shuffle.java
@@ -1,25 +1,33 @@
-//package collections;
-//
-//import java.util.List;
-//
-//public class Shuffle {
-//    public static void main(String args[]) {
-//        List<String> l = ...
-//
-//        // ...
-//    }
-//}
-//
-//
-//
-///*
-//
-//Bei Eingabe von
-//  java Shuffle i walk the line
-//
-//liefert das Programm z.B. die Ausgabe:
-//
-//[walk, line, i, the]
-//
-//
-//*/
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Shuffle {
+    public static void main(String args[]) {
+        List<String> l = new ArrayList<>();
+
+        for (String s : args) {
+            l.add(s);
+        }
+
+        Collections.shuffle(l);
+
+        l.stream().forEach(e -> System.out.println(e));
+    }
+}
+
+
+
+/*
+
+Bei Eingabe von
+  java Shuffle i walk the line
+
+liefert das Programm z.B. die Ausgabe:
+
+[walk, line, i, the]
+
+
+*/
